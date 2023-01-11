ac5c4c44047b4b833b20bed2b57a0d39d1e74cc1
wrennd <dwrenn@seattleu.edu>
2023-01-11 04:46:03 -0800

    commit new changes to week 1 code

--- week_1_assignment_1_0.R
+++ week_1_assignment_1_0.R
@@ -3,27 +3,38 @@ library(haven)
 
 nfhs <- read_dta("IAHR52FL.dta")
 
+# New data frame; limited variables
 new_df <- nfhs %>%
-  select(hhid:shstruc) %>%
-  rename(survey_month = hv006) %>%
-  filter(survey_month == 1) %>%
-  mutate(rural = hv025 == 2)
-glimpse(nfhs)
+  select(hhid:shstruc)
 
-# histogram
+# histogram utilizing entire sample
 ggplot(data = nfhs) +
   geom_histogram(mapping = aes(x = hv009),binwidth = 1) +
   xlab("Number of household members")
 
-# Boxplot
+new_df %>%
+  group_by(rural) %>%
+  count()
+
+# Convert labelled vector to a factor
+new_df2$hv026 <- haven::as_factor(new_df2$hv026)
+
+# New data frame: only urban households
 new_df2 <- nfhs %>%
   select(hhid:hv208) %>%
+  filter(hv025 == 1) %>%
   mutate(urban = hv025 == 1)
 
-ggplot(nfhs, aes(x = hv026,
-                 fill = hv026)) +
-  geom_boxplot(fill = "#4271AE",
+# Boxplot: Urban area type to number of household members
+ggplot(data = new_df2, mapping = aes(hv026,
+                                     hv009)) +
+  geom_boxplot(mapping = aes(group = cut_width(hv026, 1)),
                outlier.colour = "red") +
-  ggtitle("Boxplot: Urban Household sizes") +
-  xlab("Number of Household Members") +
-  ylab("Urban Area")
+  ggtitle("Number of Household Members by Urban Area Type") +
+  labs(x = "Urban area type", y = "Number of household members")
+
+# Urban areas mean household sizes
+new_df2 %>%
+  group_by(hv026) %>%
+  summarise(mean(hv009))
+  
